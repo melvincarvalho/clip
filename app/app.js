06c5e6177b29dd92301b897e4aca4b9f523a272c
Melvin Carvalho <melvincarvalho@gmail.com>
2015-07-02 10:30:19 +0200

    logout

--- app/app.js
+++ app/app.js
@@ -25,11 +25,68 @@ var App = angular.module('Clip', [
 ]);
 
 App.controller('Main', function($scope, $http, $timeout, LxNotificationService, LxProgressService, LxDialogService) {
-    $scope.app = {};
 
     // save app configuration if it's the first time the app runs
     $scope.initApp = function() {
+      $scope.init();
     };
+
+    $scope.TLSlogin = function() {
+        $scope.loginTLSButtonText = 'Logging in...';
+        $http({
+          method: 'HEAD',
+          url: AUTHENDPOINT,
+          withCredentials: true
+        }).success(function(data, status, headers) {
+          // add dir to local list
+          var user = headers('User');
+          if (user && user.length > 0 && user.slice(0,4) == 'http') {
+            $scope.loggedIn = true;
+            $scope.user = user;
+          } else {
+            LxNotificationService.error('WebID-TLS authentication failed.');
+            console.log('WebID-TLS authentication failed.');
+          }
+          $scope.loginTLSButtonText = 'Login';
+        }).error(function(data, status, headers) {
+            LxNotificationService.error('Could not connect to auth server: HTTP '+status);
+            console.log('Could not connect to auth server: HTTP '+status);
+            $scope.loginTLSButtonText = 'Login';
+        });
+    };
+
+    $scope.save = function() {
+      alert('Coming soon...');
+    };
+
+    $scope.logout = function() {
+      $scope.init();
+    };
+
+    // set init variables
+    $scope.init = function() {
+        $scope.initialized = true;
+        $scope.loggedIn = false;
+        $scope.loginTLSButtonText = "Login";
+        // display elements object
+        $scope.show = {
+        };
+
+        // user model
+        $scope.my = {
+            config: {
+                workspaces: [],
+                availableWorkspaces: []
+            }
+        };
+
+        // chosen storage URI for the app workspace
+        $scope.storageURI = {};
+
+    };
+
+    $scope.app = {};
+    $scope.initApp();
 
 });
 
