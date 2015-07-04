e5f90ee2a1b18600201c429b8e840941fd036dbb
Melvin Carvalho <melvincarvalho@gmail.com>
2015-07-04 12:53:37 +0200

    First pages commit

---
+++ README.md
@@ -0,0 +1,19 @@
+[![Stories in Ready](https://badge.waffle.io/melvincarvalho/clip.png?label=ready&title=Ready)](https://waffle.io/melvincarvalho/clip)
+# clip
+
+[![Join the chat at https://gitter.im/melvincarvalho/clip](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/melvincarvalho/clip?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
+
+
+#clipboard world for SoLiD apps
+
+Running live here: https://melvincarvalho.github.io/clip/
+
+Quick Start for contributors
+----------------------------
+
+```
+$ git clone git://github.com/melvincarvalho/helloworld
+$ cd helloworld
+$ sudo npm -g install bower
+$ bower install
+```
---
+++ app/app.js
@@ -0,0 +1,142 @@
+var f,g;
+var db;
+
+var CHAT  = $rdf.Namespace("https://ns.rww.io/chat#");
+var CURR  = $rdf.Namespace("https://w3id.org/cc#");
+var DCT   = $rdf.Namespace("http://purl.org/dc/terms/");
+var FACE  = $rdf.Namespace("https://graph.facebook.com/schema/~/");
+var FOAF  = $rdf.Namespace("http://xmlns.com/foaf/0.1/");
+var LIKE  = $rdf.Namespace("http://ontologi.es/like#");
+var LDP   = $rdf.Namespace("http://www.w3.org/ns/ldp#");
+var MBLOG = $rdf.Namespace("http://www.w3.org/ns/mblog#");
+var OWL   = $rdf.Namespace("http://www.w3.org/2002/07/owl#");
+var PIM   = $rdf.Namespace("http://www.w3.org/ns/pim/space#");
+var RDF   = $rdf.Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#");
+var RDFS  = $rdf.Namespace("http://www.w3.org/2000/01/rdf-schema#");
+var SIOC  = $rdf.Namespace("http://rdfs.org/sioc/ns#");
+var SOLID = $rdf.Namespace("http://www.w3.org/ns/solid/app#");
+var URN   = $rdf.Namespace("urn:");
+
+var AUTHENDPOINT = "https://databox.me/";
+var PROXY = "https://rww.io/proxy.php?uri={uri}";
+var TIMEOUT = 5000;
+var DEBUG = true;
+
+var scope = {};
+var gg;
+
+$rdf.Fetcher.crossSiteProxyTemplate=PROXY;
+
+var App = angular.module('Clip', [
+  'lumx'
+]);
+
+App.config(function($locationProvider) {
+  $locationProvider
+    .html5Mode({ enabled: true, requireBase: false });
+});
+
+App.controller('Main', function($scope, $http, $location, $timeout, LxNotificationService, LxProgressService, LxDialogService) {
+
+  // save app configuration if it's the first time the app runs
+  $scope.initApp = function() {
+    $scope.init();
+  };
+
+  $scope.TLSlogin = function() {
+    $scope.loginTLSButtonText = 'Logging in...';
+    $http({
+      method: 'HEAD',
+      url: AUTHENDPOINT,
+      withCredentials: true
+    }).success(function(data, status, headers) {
+      // add dir to local list
+      var user = headers('User');
+      if (user && user.length > 0 && user.slice(0,4) == 'http') {
+        LxNotificationService.success('Login Successful!');
+        $scope.loggedIn = true;
+        $scope.user = user;
+      } else {
+        LxNotificationService.error('WebID-TLS authentication failed.');
+        console.log('WebID-TLS authentication failed.');
+      }
+      $scope.loginTLSButtonText = 'Login';
+    }).error(function(data, status, headers) {
+      LxNotificationService.error('Could not connect to auth server: HTTP '+status);
+      console.log('Could not connect to auth server: HTTP '+status);
+      $scope.loginTLSButtonText = 'Login';
+    });
+  };
+
+  $scope.save = function() {
+    var _clipboard = $scope.clipboard;
+    if (!_clipboard) {
+      LxNotificationService.error('clipboard is empty');
+      return;
+    }
+    console.log(_clipboard);
+
+
+
+    $http({
+        method: 'PUT',
+        url: $scope.storageURI,
+        withCredentials: true,
+        headers: {
+            "Content-Type": "text/turtle"
+        },
+        data: '<#this> <urn:clipboard> """' + _clipboard + '""" .',
+    }).
+    success(function(data, status, headers) {
+      LxNotificationService.success('clipboard saved');
+      $location.search('storageURI', $scope.storageURI);
+    }).
+    error(function(data, status, headers) {
+      LxNotificationService.error('could not save clipboard');
+    });
+
+  };
+
+  $scope.logout = function() {
+    $scope.init();
+    LxNotificationService.success('Logout Successful!');
+  };
+
+  // set init variables
+  $scope.init = function() {
+
+    // start in memory DB
+    g = $rdf.graph();
+    f = $rdf.fetcher(g);
+    // add CORS proxy
+    var PROXY      = "https://data.fm/proxy?uri={uri}";
+    var AUTH_PROXY = "https://rww.io/auth-proxy?uri=";
+    //$rdf.Fetcher.crossSiteProxyTemplate=PROXY;
+    var kb         = $rdf.graph();
+    var fetcher    = $rdf.fetcher(kb);
+
+
+    $scope.initialized = true;
+    $scope.loggedIn = false;
+    $scope.loginTLSButtonText = "Login";
+    // display elements object
+
+    var storageURI = 'https://clip.databox.me/Public/.clip/Public/test';
+    if ($location.search().storageURI) {
+      storageURI = $location.search().storageURI;
+    }
+
+    f.nowOrWhenFetched(storageURI, undefined, function(ok, body) {
+      var clip = g.statementsMatching(undefined, URN('clipboard'));
+      var clipboard = clip[0].object.value;
+      console.log(clipboard);
+      $scope.clipboard = clipboard;
+      $scope.storageURI = storageURI;
+    });
+
+
+  };
+
+  $scope.initApp();
+
+});
